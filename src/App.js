e359f6d481de87c03e6724e566fabb3713b00b78
Jim Löfgren <jim.lofgren@isotop.se>
2018-07-24 15:53:00 +0200

    added some more pictures

--- src/App.js
+++ src/App.js
@@ -27,8 +27,32 @@ const Picture = (props) => {
         imageUrl: "https://images.unsplash.com/photo-1467217322460-5f03dc33a28e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fd8c223fa37d553ad9c792186bf3a9a3&auto=format&fit=crop&w=1950&q=80"
     },
     {
+        text: "SÄL",
+        imageUrl: "https://images.unsplash.com/photo-1447675702052-663a7ef9eaa6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=beb1461e41cbccf1bf1d3fbf816c420a&auto=format&fit=crop&w=1350&q=80"
+    },
+    {
+        text: "KOMPIS",
+        imageUrl: "https://images.unsplash.com/photo-1488956322016-1e7554142097?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8ef40c653843b0f89434252f8671bdca&auto=format&fit=crop&w=1350&q=80"
+    },
+    {
+        text: "SPARKA",
+        imageUrl: "https://images.unsplash.com/photo-1476525223214-c31ff100e1ae?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6a868144e4ef4fb580b12aaa8010fc76&auto=format&fit=crop&w=1350&q=80"
+    },
+    {
         text: "SIMMA",
         imageUrl: "https://images.unsplash.com/photo-1506322845680-6e3e1a5b1de1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=cce7bb6598e36e284720a0799bab43da&auto=format&fit=crop&w=969&q=80"
+    },
+      {
+        text: "SPEL",
+        imageUrl: "https://images.unsplash.com/photo-1515600467781-911b35a7e539?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=017b5199627fe076e2e1c0ea21e29c19&auto=format&fit=crop&w=1350&q=80"
+    },
+    {
+        text: "SNABEL",
+        imageUrl: "https://images.unsplash.com/photo-1526824814752-c87997cd2c71?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83c1acde8f942f890a99d1c6190e2323&auto=format&fit=crop&w=1000&q=80"
+    },
+    {
+        text: "SNÖ",
+        imageUrl: "https://images.unsplash.com/photo-1453306458620-5bbef13a5bca?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4a2ecb093c96c69ec20be4729219c081&auto=format&fit=crop&w=1350&q=80"
     },
   ];
 
@@ -70,7 +94,7 @@ class Game extends React.Component {
   
   updateWinStatus = () => {
     this.setState(prevState => {
-      if (prevState.usedPictures.length === 3) {
+      if (prevState.usedPictures.length === 9) {
         return { winStatus: 'Bra jobbat', goConfetti: true };
       }
     });
